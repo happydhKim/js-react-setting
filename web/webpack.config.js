290a393d305091db95bbc2b93e9a7ba5c17f1ad3
happydhKim <happytime870616@gmail.com>
2020-01-14 23:35:58 +0900

    [ ENV ] react setting with eslint, webpack, babel
    1. react install
    2. eslint install ( airbnb style guide )
    3. webpack install
    4. babel install
    5. style install

---
+++ web/webpack.config.js
@@ -0,0 +1,50 @@
+const Path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    app: Path.resolve(__dirname, './src/index.jsx'),
+  },
+  output: {
+    path: Path.resolve(__dirname, './build'),
+    filename: 'index.bundle.js',
+  },
+  resolve: {
+    alias: {
+      '@': Path.resolve(__dirname, './src'),
+      '@@': Path.resolve(__dirname, './public/images'),
+    },
+    extensions: ['.mjs', '.js', '.jsx'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(mjs|js|jsx)$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-env', '@babel/preset-react'],
+          plugins: ['@babel/plugin-transform-runtime'],
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|jpg|jpeg|svg)$/,
+        use: ['file-loader'],
+      },
+    ],
+  },
+  devServer: {
+    port: 8000,
+    historyApiFallback: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: Path.resolve(__dirname, './public/index.html'),
+    }),
+  ],
+};
